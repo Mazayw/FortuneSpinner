e24221bf69dc47b0e08539f44c428ff81d31ca04
Ihor <mazayw@gmail.com>
2024-01-04 19:21:17 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+import './App.css';
+
+function App() {
+	return (
+		<>
+			<button>Spin</button>
+			<span className='arrow'></span>
+		</>
+	);
+}
+
+export default App;
