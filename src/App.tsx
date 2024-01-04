2b5938158403870a76f06f7dc68b681468a0b290
Ihor <mazayw@gmail.com>
2024-01-04 20:49:40 +0100

    feat: add base styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
-import { useState } from 'react';
-
 import './App.css';
-
+const prices = [1, 2, 3, 5, 6, 7, 8];
 function App() {
+	const randomColor = () =>
+		Math.floor(Math.random() * 16777215)
+			.toString(16)
+			.padStart(6, '0');
+
 	return (
-		<>
+		<div className='container'>
 			<button>Spin</button>
 			<span className='arrow'></span>
-		</>
+			<div
+				className='wheel'
+				style={
+					{
+						'--deg': `${Math.ceil(360 / prices.length)}deg`,
+					} as React.CSSProperties
+				}
+			>
+				{prices.map((el, index) => {
+					const color = randomColor();
+					const style = {
+						'--i': index,
+						'--clr': `#${color}`,
+					} as React.CSSProperties;
+					return (
+						<div className={`sector`} style={style} key={el}>
+							<span>{el}</span>
+						</div>
+					);
+				})}
+			</div>
+		</div>
 	);
 }
 
