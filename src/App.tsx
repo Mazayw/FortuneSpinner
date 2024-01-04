e1e248e60fc778666d9cf2ea82f222dca9790597
Ihor <mazayw@gmail.com>
2024-01-04 22:52:55 +0100

    fix: small fixes

--- src/App.tsx
+++ src/App.tsx
@@ -5,43 +5,58 @@ import { Circle } from './Circle';
 
 function App() {
 	const [degree, setDegree] = useState(0);
+	const [rotates, setRotates] = useState(5);
+	const [prize, setPrize] = useState(prices[0].value);
 
 	function getRandomValueWithProbability(pricesData: typeof prices) {
 		const random = Math.random();
 		let cumulativeProbability = 0;
-		for (const price of pricesData) {
+		for (let i = 0; i <= pricesData.length; i++) {
+			const price = pricesData[i];
 			cumulativeProbability += price.probability;
 			if (random <= cumulativeProbability) {
-				return { priceValue: price.value, cumulativeProbability };
+				return {
+					priceValue: price.value,
+					cumulativeProbability: (360 / pricesData.length) * i,
+				};
 			}
 		}
-		return { priceValue: prices[0].value, cumulativeProbability: 0 };
+		return {
+			priceValue: prices[0].value,
+			cumulativeProbability: 360 / pricesData.length,
+		};
 	}
 
 	const rotate = () => {
 		const randomValue = getRandomValueWithProbability(prices);
+		const degreeNew = randomValue;
 		setDegree(
-			(prev) => prev + (randomValue?.cumulativeProbability || 0) * 360 + 360 * 5
+			(prev) => prev * 0 + degreeNew.cumulativeProbability + 360 * rotates
 		);
-		console.log(randomValue);
+		setRotates((prev) => prev + 5);
+		setPrize(randomValue.priceValue);
 	};
 
 	return (
-		<div className='container'>
-			<button onClick={rotate}>Spin</button>
-			<span className='arrow'></span>
-			<div
-				className='wheel'
-				style={
-					{
-						'--deg': `${Math.ceil(360 / prices.length)}deg`,
-						transform: `rotate(${degree}deg)`,
-					} as React.CSSProperties
-				}
-			>
-				<Circle />
+		<>
+			{' '}
+			<h1>{prize}</h1>
+			<div className='container'>
+				<button onClick={rotate}>Spin</button>
+				<span className='arrow'></span>
+				<div
+					className='wheel'
+					style={
+						{
+							'--deg': `${Math.ceil(360 / prices.length)}deg`,
+							transform: `rotate(-${degree}deg)`,
+						} as React.CSSProperties
+					}
+				>
+					<Circle />
+				</div>
 			</div>
-		</div>
+		</>
 	);
 }
 
